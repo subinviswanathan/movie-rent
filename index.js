2715efbcf9698f7f7a3c259287b5273a5da1e995
subinviswanathan <subinv1992@gmail.com>
2020-06-12 14:17:06 +0530

    mongon error use winston and uncaught exception

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
 require('express-async-errors');
 const winston = require('winston');
+require('winston-mongodb');
 const genres = require('./routes/genres');
 const customers = require('./routes/customer');
 const movies = require('./routes/movies');
@@ -13,6 +14,24 @@ const auth = require('./routes/auth');
 const config = require('config');
 const error = require('./middleware/error');
 const app = express();
+
+process.on('uncaughtException', () => {
+	console.log('WE GOT AN UNCAUGHT EXCEPTION...');
+	winston.error(ex.message, ex);
+});
+
+winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+winston.add(
+	new winston.transports.MongoDB({
+		db: 'mongodb://localhost/movie-rent',
+		options: {
+			useUnifiedTopology: true,
+			poolSize: 2,
+			useNewUrlParser: true
+		},
+		level: 'error'
+	})
+);
 // if (!config.get('jwtPrivateKey')) {
 // 	console.error('Fatal error. JWT private key not defined...');
 // 	process.exit(1);
@@ -28,7 +47,6 @@ mongoose
 	.then(() => console.log('Connected to MongoDB...'))
 	.catch(err => console.error(err));
 
-winston.add(winston.transports.File, { filename: 'logfile.log' });
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
