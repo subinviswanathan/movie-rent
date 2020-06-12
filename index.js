c64e5fe7a23286c8d780e99b8f3f81c26a8517f2
subinviswanathan <subinv1992@gmail.com>
2020-06-12 11:42:49 +0530

    adding the authentication

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const customers = require('./routes/customer');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const app = express();
 
 mongoose
@@ -25,6 +26,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 
