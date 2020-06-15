d0806ae57a75a6a57f754619977110a9921621a8
subinviswanathan <subinv1992@gmail.com>
2020-06-15 13:10:26 +0530

    create a middleware for object id

--- routes/genres.js
+++ routes/genres.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Genre, validate } = require('../models/genre');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
-const mongoose = require('mongoose');
+const validateObjectId = require('../middleware/validateObjectId');
 
 const router = express.Router();
 
@@ -11,10 +11,7 @@ router.get('/', async (req, res) => {
 	res.send(genres);
 });
 
-router.get('/:id', async (req, res) => {
-	if (!mongoose.Types.ObjectId.isValid(req.params.id))
-		return res.status(404).send('Invalid ID...');
-
+router.get('/:id', validateObjectId, async (req, res) => {
 	const genre = await Genre.findById(req.params.id);
 
 	if (!genre) return res.status(404).send('Genre for given id is not present');
