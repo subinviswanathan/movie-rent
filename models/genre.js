6ed2b79beb42738bb62ee204b6150f4c698d51ec
subinviswanathan <subinv1992@gmail.com>
2020-06-15 13:27:04 +0530

    tests for post the genre

--- models/genre.js
+++ models/genre.js
@@ -7,7 +7,7 @@ const genreSchema = mongoose.Schema({
 		trim: true,
 		required: true,
 		minlength: 5,
-		maxlength: 60
+		maxlength: 50
 	}
 });
 
@@ -15,7 +15,7 @@ const Genre = new mongoose.model('Genre', genreSchema);
 
 const validateGenre = genre => {
 	let schema = Joi.object({
-		name: Joi.string().min(5).required()
+		name: Joi.string().min(5).max(50).required()
 	});
 	return schema.validate(genre);
 };
